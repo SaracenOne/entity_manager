13f5b52ab83a4c2417bc76bcd29683938a975d6b
Saracen <SaracenOne@gmail.com>
2018-07-31 02:20:20 +0100

    Fix transform notifications switch statement.

--- transform_notification.gd
+++ transform_notification.gd
@@ -3,8 +3,9 @@ extends Spatial
 signal transform_changed()
 
 func _notification(p_notification):
-	if NOTIFICATION_TRANSFORM_CHANGED:
-		emit_signal("transform_changed")
+	match p_notification:
+		NOTIFICATION_TRANSFORM_CHANGED:
+			emit_signal("transform_changed")
 		
 func _ready():
 	set_notify_transform(true)
